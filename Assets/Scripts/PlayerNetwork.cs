bd9efcd8d2eb14c77ea700d3736918ca630dd9c0
Evan Huang <71423494+huanh8@users.noreply.github.com>
2023-02-27 16:14:16 +1300

    set up the project

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerNetwork : NetworkBehaviour {
+
+    private void Update()
+    {
+        Movement();
+    }
+    private void Movement(){
+
+        if  (!IsOwner)    return;
+        Vector3 moveDir = new Vector3 (0,0,0);
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+        float moveSpeed = 3f;
+        transform.position += moveDir*moveSpeed* Time.deltaTime;
+    }
+}
