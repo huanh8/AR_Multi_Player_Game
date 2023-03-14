06a07f1f6cb6bfc2ecf4cefb7be73ded370b3eaa
Evan Huang <huanh8@users.noreply.github.com>
2023-03-14 15:10:20 +1300

    optimize boardGenerator

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class BoardGenerator : MonoBehaviour
 {
-    [SerializeField]
-    int _size = 6;
+    const int BOARD_SIZE = 6;
     [SerializeField]
     Vector3 _rotation = new Vector3(90, 0, 45);
 
-    [SerializeField] Piece[,] _pieces = new Piece[6, 6];   // 2D array of pieces
+    [SerializeField] Piece[,] _pieces = new Piece[BOARD_SIZE, BOARD_SIZE];   // 2D array of pieces
     [SerializeField] GameObject _redPiecePrefab;          // red piece prefab
     [SerializeField] GameObject _bluePiecePrefab;        // black piece prefab
 
@@ -21,12 +20,10 @@ public class BoardGenerator : MonoBehaviour
 
     private void CreateBoard()
     {
-        int s = 3;
-        int b = -2;
-
-
+        int redRowLimit = 3;
+        int blueRowLimit = -2;
         bool isWhite = false;
-        for (int i = 0; i < _size; i++)
+        for (int i = 0; i < BOARD_SIZE; i++)
         {
             GameObject row = new GameObject();
             row.transform.SetParent(transform);
@@ -34,68 +31,42 @@ public class BoardGenerator : MonoBehaviour
 
             row.name = $"Row{i}";
             isWhite = !isWhite;
-            for (int j = 0; j < _size; j++)
+            for (int j = 0; j < BOARD_SIZE; j++)
             {
                 GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
                 quad.transform.SetParent(row.transform);
                 quad.transform.localPosition = new Vector3(0, j, 0);
                 quad.name = $"R{i}{j}";
 
-                // if isWhite is true, set quad color to white
-                if (isWhite)
-                {
-                    quad.GetComponent<Renderer>().material.color = Color.white;
-                    isWhite = false;
-                }
-                // if isWhite is false, set quad color to black
-                else
-                {
-                    quad.GetComponent<Renderer>().material.color = Color.black;
-                    isWhite = true;
-                }
+                // set quad color
+                quad.GetComponent<Renderer>().material.color = isWhite ? Color.white : Color.black;
+                isWhite = !isWhite;
 
                 // create red pieces when {0,1}, {0,1}, {0,2},{1,0},{1,1},{2,0}
-                if (j < s)
+                if (j < redRowLimit)
                 {
-                    _pieces[i, j] = Instantiate(_redPiecePrefab, new Vector3(i, j, 0), Quaternion.identity, quad.transform).GetComponent<Piece>();
+                    GeneratePieces(i, j, quad, _redPiecePrefab);
                 }
                 // create blue pieces when {3,5},{4,4},{4,5},{5,3},{5,4},{5,5}
-                else if (j >= _size - b)
+                else if (j >= BOARD_SIZE - blueRowLimit)
                 {
-                    _pieces[i, j] = Instantiate(_bluePiecePrefab, new Vector3(i, j, 0), Quaternion.identity, quad.transform).GetComponent<Piece>();
+                    GeneratePieces(i, j, quad, _bluePiecePrefab);
                 }
 
             }
-            s--;
-            b++;
+            redRowLimit--;
+            blueRowLimit++;
         }
+        RotateBoard();
+    }
+
+    private void RotateBoard()
+    {
         transform.localRotation = Quaternion.Euler(_rotation);
     }
 
-    // private void GeneratePieces()
-    // {
-    //     int s = 3;  
-    //     int b = -2;
-
-    //     for (int i = 0; i < _size; i++)
-    //     {
-    //         for (int j = 0; j < _size; j++)
-    //         {
-    //             // create red pieces when {0,1}, {0,1}, {0,2},{1,0},{1,1},{2,0}
-    //             if ( j < s)
-    //             {
-    //                 _pieces[i, j] = Instantiate(_redPiecePrefab, new Vector3(i, 0, j), Quaternion.Euler(_rotation)).GetComponent<Piece>();
-    //             }
-    //             // create blue pieces when {3,5},{4,4},{4,5},{5,3},{5,4},{5,5}
-    //             else if (j >= _size - b)
-    //             {
-    //                 _pieces[i, j] = Instantiate(_bluePiecePrefab, new Vector3(i, 0, j), Quaternion.Euler(_rotation)).GetComponent<Piece>();
-    //             }
-    //         }
-    //         s--;
-    //         b++;
-    //     }
-    // }
-
-
+    private void GeneratePieces(int i, int j, GameObject quad, GameObject piecePrefab)
+    {
+        _pieces[i, j] = Instantiate(piecePrefab, new Vector3(i, j, 0), Quaternion.identity, quad.transform).GetComponent<Piece>();
+    }
 }
