84695f09af719860c12efdc93ca31828e25970b7
Evan Huang <huanh8@users.noreply.github.com>
2023-03-16 11:03:06 +1300

    replace quad to cube and add mousePosition

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -6,16 +6,45 @@ public class BoardGenerator : MonoBehaviour
 {
     const int BOARD_SIZE = 6;
     [SerializeField]
-    Vector3 _rotation = new Vector3(90, 0, 45);
+    Vector3 _rotation = new Vector3(0, 0, 45);
 
     [SerializeField] Piece[,] _pieces = new Piece[BOARD_SIZE, BOARD_SIZE];   // 2D array of pieces
     [SerializeField] GameObject _redPiecePrefab;          // red piece prefab
     [SerializeField] GameObject _bluePiecePrefab;        // black piece prefab
-
-
+    Vector3 _mouseOver;                         // mouse over position
+    public Camera _camera;                      // camera in the scene
+    [SerializeField] GameObject _boardCube;        // board cube object
+    [SerializeField] float _offSite = -0.6f;        // offset for pieces
     public void ManualStart()
     {
         CreateBoard();
+        _camera = GameObject.Find("Camera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        UpdateMouseOver();
+        Debug.Log(_mouseOver);
+    }
+
+    private void UpdateMouseOver()
+    {
+        if(_camera == null)
+        {
+            Debug.Log("No camera found");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            //cast it as int to get the whole number
+            _mouseOver = new Vector3((int)hit.point.x, (int)hit.point.y, (int)hit.point.z);
+        }
+        else
+        {
+            _mouseOver = new Vector3(-1, -1, -1);
+        }
     }
 
     private void CreateBoard()
@@ -33,11 +62,10 @@ public class BoardGenerator : MonoBehaviour
             isWhite = !isWhite;
             for (int j = 0; j < BOARD_SIZE; j++)
             {
-                GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                GameObject quad = Instantiate(_boardCube, new Vector3(i, j, 0), Quaternion.identity);
                 quad.transform.SetParent(row.transform);
-                quad.transform.localPosition = new Vector3(0, j, 0);
+                //quad.transform.localPosition = new Vector3(0, j, 0);
                 quad.name = $"R{i}{j}";
-
                 // set quad color
                 quad.GetComponent<Renderer>().material.color = isWhite ? Color.white : Color.black;
                 isWhite = !isWhite;
@@ -67,6 +95,6 @@ public class BoardGenerator : MonoBehaviour
 
     private void GeneratePieces(int i, int j, GameObject quad, GameObject piecePrefab)
     {
-        _pieces[i, j] = Instantiate(piecePrefab, new Vector3(i, j, 0), Quaternion.identity, quad.transform).GetComponent<Piece>();
+        _pieces[i, j] = Instantiate(piecePrefab, new Vector3(i, j, _offSite), Quaternion.identity, quad.transform).GetComponent<Piece>();
     }
 }
