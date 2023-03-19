25248ec019cf2764adc6cc6ba9bd5ed883a59fdf
Evan Huang <huanh8@users.noreply.github.com>
2023-03-20 12:22:39 +1300

    update position and rotation

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -1,40 +1,55 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 
 public class BoardGenerator : MonoBehaviour
 {
-    const int BOARD_SIZE = 6;
-    [SerializeField] Vector3 _rotation = new Vector3(0, 0, 45);
 
-    [SerializeField] Piece[,] _pieces = new Piece[BOARD_SIZE, BOARD_SIZE];   // 2D array of pieces
-    // [SerializeField] GameObject _redPiecePrefab;          // red piece prefab
-    // [SerializeField] GameObject _bluePiecePrefab;        // black piece prefab
+    [SerializeField] Vector3 _pieceRotation = new Vector3(90, 0, 0);
+    [SerializeField] Piece[,] _pieces = new Piece[Constants.BOARD_SIZE, Constants.BOARD_SIZE];   // 2D array of pieces
+    [SerializeField] Material _redMaterial;
+    [SerializeField] Material _blueMaterial;
+    [SerializeField] LayerMask _layerMask;
+    [SerializeField] GameObject _boardCube;        // board cube object
+    [SerializeField] float _offSite = 0.6f;        // offset for pieces
+
+    // size of the box collider
+    [SerializeField] private float _boxColliderSize = 6f;
+    //center of the box collider
+    [SerializeField] private float _boxColliderCenter = 2.5f;
+
+    // private
     Vector3 _mouseOver;                         // mouse over position
     public Camera _camera;                      // camera in the scene
-    [SerializeField] GameObject _boardCube;        // board cube object
-    [SerializeField] float _offSite = -0.6f;        // offset for pieces
-
     private Piece _selectedPiece;               // selected piece
     private Vector3 _startDrag;                 // start drag position
     private Vector3 _endDrag;                   // end drag position
     private Piece.Factory _pieceFactory;
-    [SerializeField] Material _redMaterial;
-    [SerializeField] Material _blueMaterial;
 
     [Inject]
     private void Init(
         Piece.Factory pieceFactory
-    ){
+    )
+    {
         _pieceFactory = pieceFactory;
     }
-
 
     public void ManualStart()
     {
         CreateBoard();
-        _camera = GameObject.Find("Camera").GetComponent<Camera>();
+        //check if the _layerMask is set, if not set it to the default layer
+        _layerMask = _layerMask == 0 ? LayerMask.GetMask(Constants.BOARD_NAME) : _layerMask;
+        _camera = GameObject.Find(Constants.CAMERA_NAME).GetComponent<Camera>();
+        //set box collider size
+        SetSizeBoxCollider();
+    }
+
+    private void SetSizeBoxCollider()
+    {
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+        boxCollider.size = new Vector3(_boxColliderSize, 1, _boxColliderSize);
+        boxCollider.center = new Vector3(_boxColliderCenter, 0, _boxColliderCenter);
     }
 
     private void Update()
@@ -48,9 +63,9 @@ public class BoardGenerator : MonoBehaviour
 
             if (Input.GetMouseButtonDown(0))
             {
-               SelectPiece(x,z);
+                SelectPiece(x, z);
             }
-            if(Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonUp(0))
             {
                 TryMove((int)_startDrag.x, (int)_startDrag.z, x, z);
             }
@@ -58,7 +73,7 @@ public class BoardGenerator : MonoBehaviour
     }
     private void TryMove(int x1, int z1, int x2, int z2)
     {
-       _startDrag = new Vector3(x1, 0, z1);
+        _startDrag = new Vector3(x1, 0, z1);
         _endDrag = new Vector3(x2, 0, z2);
         _selectedPiece = _pieces[x1, z1];
         Debug.Log($"try move _selectedPiece.name {_selectedPiece.name} {x2}, {z2}");
@@ -74,19 +89,19 @@ public class BoardGenerator : MonoBehaviour
         {
             _selectedPiece = p;
             _startDrag = _mouseOver;
-            Debug.Log($"_selectedPiece.name {_selectedPiece.name} {x}, {z}" );
+            Debug.Log($"_selectedPiece.name {_selectedPiece.name} {x}, {z}");
         }
     }
     private void UpdateMouseOver()
     {
-        if(_camera == null)
+        if (_camera == null)
         {
             Debug.Log("No camera found");
             return;
         }
 
         RaycastHit hit;
-        if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, _layerMask))
         {
             //cast it as int to get the whole number
             _mouseOver = new Vector3((int)hit.point.x, (int)hit.point.y, (int)hit.point.z);
@@ -102,7 +117,7 @@ public class BoardGenerator : MonoBehaviour
         int redRowLimit = 3;
         int blueRowLimit = -2;
         bool isWhite = false;
-        for (int i = 0; i < BOARD_SIZE; i++)
+        for (int i = 0; i < Constants.BOARD_SIZE; i++)
         {
             GameObject row = new GameObject();
             row.transform.SetParent(transform);
@@ -110,9 +125,9 @@ public class BoardGenerator : MonoBehaviour
 
             row.name = $"Row{i}";
             isWhite = !isWhite;
-            for (int j = 0; j < BOARD_SIZE; j++)
+            for (int j = 0; j < Constants.BOARD_SIZE; j++)
             {
-                GameObject quad = Instantiate(_boardCube, new Vector3(i, j, 0), Quaternion.identity);
+                GameObject quad = Instantiate(_boardCube, new Vector3(i, 0, j), Quaternion.identity);
                 quad.transform.SetParent(row.transform);
                 //quad.transform.localPosition = new Vector3(0, j, 0);
                 quad.name = $"R{i}{j}";
@@ -124,10 +139,10 @@ public class BoardGenerator : MonoBehaviour
                 if (j < redRowLimit)
                 {
                     GeneratePieces(i, j, quad, _redMaterial);
-                   
+
                 }
                 // create blue pieces when {3,5},{4,4},{4,5},{5,3},{5,4},{5,5}
-                else if (j >= BOARD_SIZE - blueRowLimit)
+                else if (j >= Constants.BOARD_SIZE - blueRowLimit)
                 {
                     // GeneratePieces(i, j, quad, _bluePieceFactory);
                     GeneratePieces(i, j, quad, _blueMaterial);
@@ -137,17 +152,10 @@ public class BoardGenerator : MonoBehaviour
             redRowLimit--;
             blueRowLimit++;
         }
-        RotateBoard();
-    }
-
-    private void RotateBoard()
-    {
-        transform.localRotation = Quaternion.Euler(_rotation);
     }
 
     private void GeneratePieces(int i, int j, GameObject quad, Material material)
     {
-        // _pieces[i, j] = Instantiate(piecePrefab, new Vector3(i, j, _offSite), Quaternion.identity, quad.transform).GetComponent<Piece>();
-        _pieces[i, j] = _pieceFactory.Create(new Vector3(i, j, _offSite), Quaternion.identity, quad.transform, material);
+        _pieces[i, j] = _pieceFactory.Create(new Vector3(i, _offSite, j), Quaternion.Euler(_pieceRotation), quad.transform, material);
     }
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -7,12 +7,13 @@ public class Piece : MonoBehaviour
 {
 
     [Inject]
-    private void initialize(Vector3 vec3, Quaternion quat, Transform trans, Material mat)
+    private void init(Vector3 vec3, Quaternion quat, Transform trans, Material mat)
     {
         transform.localPosition = vec3;
         transform.SetParent(trans);
         // change the material
         GetComponent<Renderer>().material = mat;
+        transform.localRotation = quat;
     }
 
  public class Factory : PlaceholderFactory<Vector3,Quaternion,Transform, Material,Piece> { }
